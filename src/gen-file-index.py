60a07023f56babe79fdb25fbc0ffcdf0dda6b8c2
ac-yu <amyyu123@hotmail.com>
2023-03-07 03:08:07 -0800

    Image and file descriptions

--- src/gen-file-index.py
+++ src/gen-file-index.py
@@ -2,35 +2,71 @@ import sys
 import os
 from os.path import isdir
 import json
+import re
+import html
 
 dataPath = 'data/'
 
-def getTitle(directory):
-    title = directory.replace('-', ' ')
-    title = title.replace('_', '')
-    title = ''.join([i for i in title if not i.isdigit()]).strip()
-    return title
+def getFileMetadata(metadata, directory, nameKey):
+    dir = re.sub('[^a-zA-Z0-9]+', '', directory)
+    print(directory, dir)
+    for md in metadata:
+        name = re.sub('[^a-zA-Z0-9]+', '', html.unescape(md[nameKey]))
+        print('-', md[nameKey], name)
+        if dir.endswith(name):
+            return md
+            
 
-def getListing(type, path, file):
+def getPhotoMetadata(metadata, file):
+    for md in metadata:
+        if file.startswith(str(md['photoId'])):
+            return md
+
+
+def getListing(type, path, file, md):
+    print(path)
     if type == 'files':
-        return '<a href="'+path+'">'+file+'</a><br/>\n'
-    
-    return '<a href="'+path+'"><img src="'+path+'" alt="'+file+'" height=150 /></a>\n'
+        return '<a href="'+path+'">'+md['fileName']+'</a> - '+md['description']+'<br/>\n'
+        
+    desc = ''
+    if 'description' in md:
+        desc = md['description']
+    return '<table border=0><tr><td><a href="'+path+'"><img src="'+path+'" alt="'+file+'" height=150 /></a></td><td style="max-width: 100px;"><div style="max-height: 150px; overflow-y: auto; word-wrap: break-word;">'+desc+'</div></td></tr><tr><td style="word-wrap: break-word; width: 100px;">'+md['photoName']+'</td></tr></table>\n'
 
 
-def getFilesInDirectory(type, content, group, path, dir, lev):
+def getFilesInDirectory(type, content, group, path, dir, metadata, lev):
     subfiles = sorted(os.listdir(group+path), key=lambda f: f if f.find('_') == 2 else '0'+f)
     if len(subfiles) <= 1:
         return content, False
         
-    content = content + '<h'+str(lev)+' id="'+dir+'">' + getTitle(dir) + '</h'+str(lev)+'>\n'
+    if type == 'files':
+        metadataFile = open(group+path+'/fileinfo.json', "r", encoding="utf8")
+        nameKey = 'fileName'
+    else:
+        metadataFile = open(group+path+'/photos-0.json', "r", encoding="utf8")
+        nameKey = 'albumName'
+    mds = json.load(metadataFile)
+        
+    print(metadata, dir)
+    content = content + '<h'+str(lev)+' id="'+dir+'">' + metadata[nameKey] + '</h'+str(lev)+'>\n'
+    content = content + '<p>'+metadata['description']+'</p>\n'
+    if type == 'photos':
+        content = content+'<div style="display: flex; flex-direction: row; flex-wrap: wrap;">\n'
+    
     for subfile in subfiles:
         if subfile.endswith('.json'):
             continue
-        if isdir(group+path+'/'+subfile):
-            content, added = getFilesInDirectory(type, content, group, path+'/'+subfile, subfile, lev+1)
+        if type == 'files':
+            md = getFileMetadata(mds, subfile, nameKey)
         else:
-            content = content + getListing(type, path+'/'+subfile, subfile)
+            md = getPhotoMetadata(mds, subfile)
+        
+        if isdir(group+path+'/'+subfile):
+            content, added = getFilesInDirectory(type, content, group, path+'/'+subfile, subfile, metadata, lev+1)
+        else:
+            content = content + getListing(type, path+'/'+subfile, subfile, md)
+    if type == 'photos':
+        content = content+'</div>\n'
     content = content + '<br/>\n'
             
     return content, True
@@ -38,8 +74,17 @@ def getFilesInDirectory(type, content, group, path, dir, lev):
 def gen(type, group):
     templateFile = open('templates/'+type+'.html', "r", encoding="utf8")
     template = templateFile.read()
+    
+    if type == 'files':
+        metadataFile = open(group+'data/files/fileinfo.json', "r", encoding="utf8")
+        nameKey = 'fileName'
+    else:
+        metadataFile = open(group+'data/photos/albums.json', "r", encoding="utf8")
+        nameKey = 'albumName'
+    metadata = json.load(metadataFile)
 
     content = ''
+    nav = ''
     directories = []
     files = sorted(os.listdir(group+dataPath+type), key=lambda f: f if f.find('_') == 2 else '0'+f)
     for file in files:
@@ -47,17 +92,14 @@ def gen(type, group):
             continue
             
         path = dataPath+type+'/'+file
+        md = getFileMetadata(metadata, file, nameKey)
         if isdir(group+path):
-            content, added = getFilesInDirectory(type, content, group, path, file, 3)
+            content, added = getFilesInDirectory(type, content, group, path, file, md, 3)
             if added:
-                directories.append(file)
+                nav = nav + '<li>'+'<a href="#'+file+'">'+md[nameKey]+'</a> - '+md['description']+'</li>\n'
         else:
-            content = content + getListing(type, path, file)
-            
-    nav = ''
-    for dir in directories:
-        nav = nav + '<li>'+'<a href="#'+dir+'">'+getTitle(dir)+'</a></li>\n'
-    
+            content = content + getListing(type, path, file, md)
+
     output = template.replace('{{group}}', group.replace('_', ' ').replace('/',' '))
     output = output.replace('{{table-of-contents}}', nav)
     output = output.replace('{{list}}', content)
